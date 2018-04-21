d54714be89559fcdb25b749b7bacfdc19551f077
y2k6704 <dcamilien4@gmail.com>
2018-04-21 18:18:39 -0400

    Add files via upload

---
+++ Score.py
@@ -0,0 +1,53 @@
+import random
+
+class Item:
+    def __init__(self, cost):
+        self.cost = cost
+
+class Person:
+    def __init__(self, item , require, trade):
+        self.item = Item
+        self.require = Item
+        self.trade = False
+
+    def trade(item, require):
+        placeholder = 0
+        item = plceholder
+        placeholder = require
+        item = placeholder
+        del placeholder
+        
+    def behavior(item, require):
+        if item.cost >= require.cost:
+            #50 / 50 
+            trade = False
+        if item.cost == require.cost:
+            trade(item, require)
+        if item.cost <= require.cost:
+            trade(item, require)
+        trade = False
+
+def List():
+    #random list of people and items
+    for i in range(0, 10):
+        a = random.randint(0, 50)
+        print(a)
+    
+
+    print
+
+        
+    return 0
+
+def algo():
+    #solves this problem
+    #gives it 30  iterations
+    return 0
+
+def run():
+    #runs the algorithm 
+    
+    
+    return 0
+    
+        
